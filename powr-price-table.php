5d25b1f7da13f4db05202b1a201a26cdcdcc4617
powr <powr@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-09 18:47:29 +0000

    Updates
    git-svn-id: https://plugins.svn.wordpress.org/powr-price-table/trunk@1177793 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- powr-price-table.php
+++ powr-price-table.php
@@ -1,14 +1,14 @@
 <?php
     /**
      * @package POWr Price Table
-     * @version 1.4.001
+     * @version 1.4.002
      */
     /*
     Plugin Name: POWr Price Table
     Plugin URI: http://www.powr.io
     Description: Showcase pricing for products, plans, and features side-by-side!  Drop the widget anywhere in your theme. Or use the POWr icon in your WP text editor to add to a page or post. Edit on your live page by clicking the settings icon. More plugins and tutorials at POWr.io.
     Author: POWr.io
-    Version: 1.4.001
+    Version: 1.4.002
     Author URI: http://www.powr.io
     */
 
